7962fdde46887caa399dda25815bb54ba7c6f06a
fernandoalmeid <fernandomatos08665@gmail.com>
2026-05-05 08:45:55 +0100

    start

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// backend/server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/pos");
+
+app.get("/", (req, res) => {
+  res.send("POS API running");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
